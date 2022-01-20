765ac347a11482ccc3d3d2a057dba3ada3ffdc9d
Gleb Shkut <gleb.shkut@gmail.com>
2022-01-19 19:18:02 -0800

    wordSearch

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -4,7 +4,21 @@ const assert = chai.assert;
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+  it("return false empty word input",() => {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '');
+    assert.isFalse(result);
+  });
+  it("should return false if the word is not present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -20,7 +34,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -35,4 +49,37 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'A', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'N', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'N', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'A', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'WEFMHANNA')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'A', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'N', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'N', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'A', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'HANNA')
+
+    assert.isFalse(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    
+    let result = false;
+
+    // check empty word input
+    if (word.length === 0) {
+        return result;
     }
+    
+    // check word horizontally
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (let l of horizontalJoin) {
+        if (l.includes(word)) {
+            result = true;
+        }
+    }
+
+    // check word vertically
+    let string = '';
+    for (let column = 0; column < letters[0].length; column++) {
+        for (let row = 0; row < letters.length; row++) {
+            string += letters[row][column];
+        }
+        if (string === word) {
+            result = true;
+        }
+        string = '';
+    }
+
+    return result;
 }
 
 module.exports = wordSearch
